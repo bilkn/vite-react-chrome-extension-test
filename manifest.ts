174dd6db8f88199a76426167fc3b3e2f635c54f2
bilkn <bilkan.konus@gmail.com>
2023-09-26 13:26:34 +0300

    remove content

--- manifest.ts
+++ manifest.ts
@@ -5,10 +5,9 @@ import packageJson from "./package.json";
  */
 const manifest: chrome.runtime.ManifestV3 = {
   manifest_version: 3,
-  name: packageJson.name,
+  name: "Timezone Clox",
   version: packageJson.version,
   description: packageJson.description,
-  permissions: ["storage", "scripting"],
   background: {
     service_worker: "src/pages/background/index.js",
     type: "module",
